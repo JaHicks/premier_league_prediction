b0105998a5b352e9bcb6d9866d0a9a50ad7f17e0
Jacob Hicks <jacob.arran.hicks@gmail.com>
2021-01-26 16:54:27 +0000

    Initial commit using cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for predicting premier league football scores',
+    author='Jacob Hicks',
+    license='MIT',
+)
